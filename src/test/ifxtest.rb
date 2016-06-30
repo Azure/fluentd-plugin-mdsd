1c11638542bb1635f94914c7015da880341fceff
liweip <liwei.peng@microsoft.com>
2016-06-30 16:00:38 -0700

    fluentd IfxMetrics API output plugin initial design

---
+++ src/test/ifxtest.rb
@@ -0,0 +1,30 @@
+#!/opt/microsoft/omsagent/ruby/bin/ruby
+
+# Set tracing information for IFX API and RtcPal
+ENV["RTCPAL_LOG_LEVEL"] = "2"
+ENV["RTCPAL_LOG_IDENT"] = "ruby"
+
+require 'socket'
+require '../ifxext/ifxext'
+
+testAccount = "AzLinux"
+testNameSpace = "RubyIfxExt"
+testMetric = "% Idle Time"
+dim1 = "Hostname"
+dim2 = "ProcessorName"
+
+myComputename = Socket.gethostname()
+
+x = Ifxext::Mdm2D.new(testAccount, testNameSpace, testMetric, dim1, dim2)
+mdmmethods = (x.methods - Object.methods)
+puts "Mdm2D methods: #{mdmmethods}"
+
+(1..4).each {|i|
+    r = x.LogValue(200+i, myComputename, "CPU" + (i%4).to_s)
+    puts "LogValue #{i} returns #{r}"
+    sleep(1)
+
+    r2 = x.LogValueAtTime(131116247046770000, 200+i, myComputename, "CPU" + (i%4).to_s)
+    puts "LogValueAtTime #{i} returns #{r}"
+    sleep(1)
+}
