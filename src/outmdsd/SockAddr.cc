f8547e2c81c4823804b75edeaedba1ad232288c2
Liwei Peng <liweip@microsoft.com>
2016-11-29 21:22:10 +0000

    Merged PR 98001: Implement SockAddr for socket ops; DjsonLogItem for djson protocol
    This PR includes 1) SockAddr class for socket() programming (Tcp socket or unix domain socket). 2) DjsonLogItem for managing items following djson protocol when talking with mdsd.

---
+++ src/outmdsd/SockAddr.cc
@@ -0,0 +1,43 @@
+#include <cstring>
+#include <stdexcept>
+
+extern "C" {
+#include <arpa/inet.h>
+}
+
+#include "SockAddr.h"
+
+using namespace EndpointLog;
+
+UnixSockAddr::UnixSockAddr(
+    const std::string & socketfile
+    )
+{
+    if (socketfile.empty()) {
+        throw std::invalid_argument("UnixSockAddr: unexpected empty socketfile parameter.");
+    }
+
+    // maxLength: maximum permitted length of a path of a Unix domain socket
+    constexpr auto maxLength = sizeof(addr.sun_path);
+    if (socketfile.size() > maxLength) {
+        throw std::invalid_argument("UnixSockAddr: socketfile '" + socketfile +
+            "' exceeds max allowed length " + std::to_string(maxLength));
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, socketfile.c_str(), maxLength);
+}
+
+TcpSockAddr::TcpSockAddr(
+    int port
+    )
+{
+    if (port <= 0) {
+        throw std::invalid_argument("TcpSockAddr: invalid port " + std::to_string(port));
+    }
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+}
