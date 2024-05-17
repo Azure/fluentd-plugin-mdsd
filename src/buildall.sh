621ef091cd790f24341eda620cd31369b5cc69f5
Aaron Marten <AaronRM@users.noreply.github.com>
2024-05-16 17:06:02 -0700

    Azure Linux 2.0 Compatibility (#81)
    * Python3 compat fixes
    * Allow setting 'system' as a target to use system-installed fluentd ruby gem
    * Update version to 0.2.0
    * Use SWIG CMake module
    * CMake min_version 3.0

--- src/buildall.sh
+++ src/buildall.sh
@@ -26,7 +26,7 @@ Usage()
     echo "    -d: build debug build."
     echo "    -h: help."
     echo "    -o: build optimized(release) build."
-    echo "    -t: fluentd target ('td' or 'oms')"
+    echo "    -t: fluentd target ('system', 'td' or 'oms')"
 }
 
 if [ "$#" == "0" ]; then
@@ -73,8 +73,8 @@ for i; do
     esac
 done
 
-if [ "${Target}" != "td" ] && [ "${Target}" != "oms" ]; then
-    echo "Error: invalid -t value. Expected: 'td' or 'oms'"
+if [ "${Target}" != "system" ] && [ "${Target}" != "td" ] && [ "${Target}" != "oms" ]; then
+    echo "Error: invalid -t value. Expected: 'system', 'td' or 'oms'"
     exit 1
 fi
 
@@ -85,6 +85,7 @@ fi
 
 FindRubyPath()
 {
+    # The full path directory containing ruby bin (e.g. 'ruby', 'gem', etc)
     if [ "${Target}" == "td" ]; then
         if [ -d "/opt/td-agent/embedded/bin" ]; then
             RubyBaseDir="/opt/td-agent/embedded/include/ruby-"
@@ -96,14 +97,19 @@ FindRubyPath()
     elif [ "${Target}" == "oms" ]; then
         RubyBaseDir="/opt/microsoft/omsagent/ruby/include/ruby-"
         RUBY_BIN_PATH=/opt/microsoft/omsagent/ruby/bin
+    elif [ "${Target}" == "system" ]; then
+        RUBY_BIN_PATH=/usr/bin
+        RUBY_INC_PATH=/usr/include
     else
         echo "FindRubyPath() error: unexpected target ${Target}."
         exit 1
     fi
 
-    for diritem in "${RubyBaseDir}"*; do
-        [ -d "${diritem}" ] && RUBY_INC_PATH="${diritem}" && break
-    done
+    if [ -z "${RUBY_INC_PATH}" ]; then
+        for diritem in "${RubyBaseDir}"*; do
+            [ -d "${diritem}" ] && RUBY_INC_PATH="${diritem}" && break
+        done
+    fi
 
     if [ -z "${RUBY_INC_PATH}" ]; then
         echo "Error: failed to get value for RUBY_INC_PATH."
@@ -125,6 +131,7 @@ BuildWithCMake()
 
     cmake -DCMAKE_C_COMPILER=${CCompiler} -DCMAKE_CXX_COMPILER=${CXXCompiler} \
           -DRUBY_INC_PATH=${RUBY_INC_PATH} \
+          -DFLUENTD_TARGET=${Target} \
           -DCMAKE_BUILD_TYPE=${BuildType} ../
 
     if [ $? != 0 ]; then
@@ -192,9 +199,21 @@ CreateGemFile()
     cp -f ../builddir/release/lib/Liboutmdsdrb.so ./lib/fluent/plugin/Liboutmdsdrb.so
     cp -f ../../LICENSE.txt ../../README.md .
 
-    sed "s/GEMVERSION/${Version}/g" gemspec-template > fluent-plugin-mdsd.gemspec
-    echo ${RUBY_BIN_PATH}/fluent-gem build fluent-plugin-mdsd.gemspec
-    ${RUBY_BIN_PATH}/fluent-gem build fluent-plugin-mdsd.gemspec
+    if [ "${Target}" == "system" ]; then
+        FluentDVersionSpec="~> 1.16.5"
+    else
+        FluentDVersionSpec="~> 0.12.0"
+    fi
+    sed "s/GEMVERSION/${Version}/g; s/FLUENTDVERSIONSPEC/${FluentDVersionSpec}/g" gemspec-template > fluent-plugin-mdsd.gemspec
+
+    # If Target is 'system', then use gem to build the gem file. Otherwise, use fluent-gem.
+    GEM_BIN=${RUBY_BIN_PATH}/fluent-gem
+    if [ "${Target}" == "system" ]; then
+        GEM_BIN=gem
+    fi
+
+    echo ${GEM_BIN} build fluent-plugin-mdsd.gemspec
+    ${GEM_BIN} build fluent-plugin-mdsd.gemspec
     if [ $? != 0 ]; then
         let TotalErrors+=1
         echo Error: CreateGemFile failed
@@ -221,7 +240,9 @@ echo Start build at `date`. BuildType=${BuildType} CC=${CCompiler} Target=${Targ
 
 FindRubyPath
 BuildWithCMake
-ParseGlibcVer
+if [ "${Target}" != "system" ]; then
+    ParseGlibcVer
+fi
 CreateGemFile
 ReleaseGemFile
 
